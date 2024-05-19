937d707dc5faf061bf92e8f336b51279d1c4b58c
Lei Wang <mail.lei.wang@gmail.com>
2024-05-19 13:29:25 +0200

    initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+    const name = '';
+    if(name) {
+        // JSX Javascript XML
+        return <h1>Hello, {name}</h1>;
+    } else {
+        return <h1>Hello, World</h1>;
+    }
+}
+export default Message;
