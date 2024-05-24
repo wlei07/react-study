34bb7ff735513aebb7f9c66d4f620b64c2e23e81
Lei Wang <lei.wang@zetes.com>
2024-05-24 22:28:23 +0200

    CSS in progress, inline styles done

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import styles from './ListGroup.module.css';
 import styled from "styled-components";
 
+// code example for styled-components, i.e., CSS in JS
 const List = styled.ul`
     list-style: none;
     padding: 0;
@@ -40,7 +41,7 @@ interface ListGroupProps {
 function ListGroup({items, heading, onSelectItem}: ListGroupProps) {
     // initialized -1 to represent at the beginning, no item is selected.
     let [selectedIndex, setSelectedIndex] = useState(-1);
-    let [selectedIndexListWithStyledComponent, setSelectedIndexListWithStyledComponent] = useState(1);
+    let [selectedIndexListWithStyledComponent, setSelectedIndexListWithStyledComponent] = useState(-1);
     return (
         // because React component could only return one element,
         // so <h1> and <ul> has to be wrapped in <>, i.e., a Fragment element.
@@ -48,7 +49,7 @@ function ListGroup({items, heading, onSelectItem}: ListGroupProps) {
             <h1>{heading}</h1>
             {/* Note in JavaScript: true && 'Lei' returns 'Lei', however, false && 'Lei' returns false */}
             {items.length === 0 && <p>No item found</p>}
-            <ul className={[styles.listGroup, styles.container].join(' ')}>
+            <ul className={[styles.listGroup, styles.container].join(' ')} style={{backgroundColor: "yellow"}}>
                 {items.map((item, index) => (
                     <li
                         className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
@@ -66,13 +67,14 @@ function ListGroup({items, heading, onSelectItem}: ListGroupProps) {
             <h1>List using CSS in JS, i.e., "styled-components" library.</h1>
             <List>
                 {items.map((item, index) => (
-                    <ListItem active={index === selectedIndexListWithStyledComponent}
-                              key={item}
+                    <ListItem
+                        active={index === selectedIndexListWithStyledComponent}
+                        key={item}
                         // onClick could provide an event: MouseEvent parameter, import {MouseEvent} from "react" in this case
-                              onClick={() => {
-                                  setSelectedIndexListWithStyledComponent(index);
-                                  onSelectItem(item);
-                              }}
+                        onClick={() => {
+                            setSelectedIndexListWithStyledComponent(index);
+                            onSelectItem(item);
+                        }}
                     >
                         {item}
                     </ListItem>
